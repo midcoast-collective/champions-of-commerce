f2d84fb5697eecc6015cc948b2e620f296990bf4
Nate Arnold <hello@natearnold.me>
2020-08-21 17:11:21 -0500

    Update og image path

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -32,12 +32,12 @@ const Layout = ({
         <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={url + "/images/og-image.jpg"} />
+        <meta property="og:image" content={url + "/images/og-image.png"} />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={url + "/images/og-image.jpg"} />
+        <meta name="twitter:image" content={url + "/images/og-image.png"} />
 
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>

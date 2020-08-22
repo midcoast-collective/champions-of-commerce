c5e68ea19ba12d046a87fdb806c1b721e545c3a5
Nate Arnold <hello@natearnold.me>
2020-08-21 21:14:07 -0500

    Set max width on branding svg

--- components/Post.tsx
+++ components/Post.tsx
@@ -16,9 +16,9 @@ const Post = ({ post }: Readonly<PostProps>) => {
       <div className="post-image">
         {/* <div className="post-image" style={{ opacity: imageHasLoaded ? 1 : 0 }}> */}
         <Img
-          // alt={post.name}
+          alt={post.name}
           // onLoad={() => setImageHasLoaded(true)}
-          loading="lazy"
+          // loading="lazy"
           src={require(`../public/images/uploads/${post.thumbnail}`)}
         />
       </div>
